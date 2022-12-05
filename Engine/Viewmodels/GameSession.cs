6eefcbd63e2caef72ce7b3cb0b3d2ddf0b7429d2
William Williams <dayzed4life@gmail.com>
2022-12-05 16:34:43 -0600

    Added Player Class and implemented Grid to show them on MainWindow.xaml

---
+++ Engine/Viewmodels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.Viewmodels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Will";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            
+
+        }
+    }
+}
